6c8c11a7f472295bf35b617d2f4ffd10f4b024d1
who0803 <10rlatkdtn@naver.com>
2025-04-09 08:02:47 +0900

    feat: Add multifly test code

--- src/__test__/multiply.test.js
+++ src/__test__/multiply.test.js
@@ -3,3 +3,7 @@ import { multiply } from "@/utils";
 test("multiplies 2 * 3 to equal 6", () => {
   expect(multiply(2, 3)).toBe(6);
 });
+
+test("multiplies 2 * 4 to equal 8", () => {
+  expect(multiply(2, 4)).toBe(8);
+});
