26be2dae833dd0fa2cee3d352e066fe4e5d6a101
who0803 <10rlatkdtn@naver.com>
2025-04-09 07:48:32 +0900

    feat: Add multiply util function and test

---
+++ src/__test__/multiply.test.js
@@ -0,0 +1,5 @@
+import { multiply } from "@/utils";
+
+test("multiplies 2 * 3 to equal 6", () => {
+  expect(multiply(2, 3)).toBe(6);
+});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { sum } from "./sum";
+export { multiply } from "./multiply";
