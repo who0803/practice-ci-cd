353304eaf7d3d4dc37342644ce09e69414001443
who0803 <10rlatkdtn@naver.com>
2025-04-09 14:22:17 +0900

    fix: ensure .next build artifacts are uploaded properly in GitHub Actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "qmueo5",
   e2e: {
     baseUrl: "http://localhost:3000", // npm start로 Next.js dev 서버 실행되니까
     supportFile: false,
